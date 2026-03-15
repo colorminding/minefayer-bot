952650f72b050ac9af49a70c59eb223290871f8c
Colourising Mindset <cyberpupking@gmail.com>
2026-03-15 21:52:33 +0100

    Set fixed camera angles to specified values

--- bot.js
+++ bot.js
@@ -19,8 +19,8 @@ const CFG = {
   attackIntervalMs: Number(process.env.ATTACK_INTERVAL_MS || 650),
   
   // Fixed camera angles
-  yaw: 0,
-  pitch: 0,
+  yaw: 136.2,
+  pitch: -74.1,
   
   exitOnDisconnect: (process.env.EXIT_ON_DISCONNECT || '1') === '1'
 }
